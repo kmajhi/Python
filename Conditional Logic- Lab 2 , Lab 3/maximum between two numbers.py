9fa49ea1905e566e835c8736a5fe7af0b0156daa
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-04-08 15:23:33 +0600

    Write a C program to find maximum between two numbers.

---
+++ Conditional Logic- Lab 2 , Lab 3/maximum between two numbers.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+max_num = num1 if num1 > num2 else num2
+
+print("Maximum between", num1, "and", num2, "is", max_num)
