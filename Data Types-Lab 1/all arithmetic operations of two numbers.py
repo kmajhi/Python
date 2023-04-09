e3dd356fbfb8d1643f70b77278cfe0334cdaf35f
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-04-09 22:32:16 +0600

    Write a C program to enter two numbers and perform all arithmetic operations.

---
+++ Data Types-Lab 1/all arithmetic operations of two numbers.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+modulus = num1 % num2
+exponentiation = num1 ** num2
+floor_division = num1 // num2
+
+print("Sum of the numbers is:", sum)
+print("Difference of the numbers is:", difference)
+print("Product of the numbers is:", product)
+print("Quotient of the numbers is:", quotient)
+print("Modulus of the numbers is:", modulus)
+print("Exponentiation of the numbers is:", exponentiation)
+print("Floor division of the numbers is:", floor_division)
