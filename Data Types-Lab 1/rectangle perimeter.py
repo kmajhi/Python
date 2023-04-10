bc860ba8facbc0c447abfe3403884035321be4e2
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-04-10 21:31:30 +0600

    Write a C program to enter length and breadth of a rectangle and find its perimeter.

---
+++ Data Types-Lab 1/rectangle perimeter.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
+
+perimeter = 2 * (length + breadth)
+
+print("The perimeter of the rectangle is:", perimeter)
