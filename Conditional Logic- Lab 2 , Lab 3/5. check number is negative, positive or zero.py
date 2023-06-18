03692e6e6671c66b9ebd11205887708e6680b552
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-06-18 20:57:25 +0600

    5. Write a python program to check whether a number is negative, positive or zero.

---
+++ Conditional Logic- Lab 2 , Lab 3/5. check number is negative, positive or zero.py
@@ -0,0 +1,11 @@
+def check_number(number):
+    if number > 0:
+        print(f"{number} is positive.")
+    elif number < 0:
+        print(f"{number} is negative.")
+    else:
+        print("The number is zero.")
+
+# Example usage
+num = float(input("Enter a number: "))
+check_number(num)
