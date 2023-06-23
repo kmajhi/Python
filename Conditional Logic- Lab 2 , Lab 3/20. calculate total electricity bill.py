1ed07f51883d064c5c0f23506c0ca1c12b821979
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-06-23 20:37:39 +0600

    20. Write a python program to input electricity unit charges and calculate total electricity bill according to the given condition:

---
+++ Conditional Logic- Lab 2 , Lab 3/20. calculate total electricity bill.py
@@ -0,0 +1,24 @@
+def calculate_electricity_bill(units):
+    if units <= 50:
+        total_bill = units * 0.50
+    elif units <= 150:
+        total_bill = 50 * 0.50 + (units - 50) * 0.75
+    elif units <= 250:
+        total_bill = 50 * 0.50 + 100 * 0.75 + (units - 150) * 1.20
+    else:
+        total_bill = 50 * 0.50 + 100 * 0.75 + 100 * 1.20 + (units - 250) * 1.50
+    
+    surcharge = total_bill * 0.20  # 20% surcharge
+    total_bill += surcharge
+    
+    return total_bill
+
+
+# Input electricity units from the user
+units = int(input("Enter the electricity units: "))
+
+# Calculate the total electricity bill
+total_bill = calculate_electricity_bill(units)
+
+# Print the result
+print("Total electricity bill: Rs.", total_bill)
