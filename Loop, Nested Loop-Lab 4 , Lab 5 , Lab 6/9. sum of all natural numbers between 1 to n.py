ba07406187730668c5039770dee3566131dc80ab
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-01 23:16:53 +0600

    9. sum of all natural numbers between 1 to n.py

--- Loop, Nested Loop-Lab 4 , Lab 5 , Lab 6/9. sum of all natural numbers between 1 to n.py
+++ Loop, Nested Loop-Lab 4 , Lab 5 , Lab 6/9. sum of all natural numbers between 1 to n.py
@@ -1,18 +1,3 @@
-def calculate_sum(n):
-    if n < 1:
-        return 0
-
-    total_sum = 0
-    for i in range(1, n + 1):
-        total_sum += i
-
-    return total_sum
-
-# Get input from the user
 n = int(input("Enter a number: "))
-
-# Calculate the sum of natural numbers
-sum_of_natural_numbers = calculate_sum(n)
-
-# Display the result
+sum_of_natural_numbers = sum(range(1, n + 1))
 print("The sum of natural numbers from 1 to", n, "is:", sum_of_natural_numbers)
